536a5da7a31e175c773786d71e63ea4299ed7cfe
Igor <w0rz@protonmail.com>
2022-09-07 16:05:40 -0300

    weather && city data retrived from API

---
+++ js/forecast.js
@@ -0,0 +1,22 @@
+const key = import.meta.env.VITE_API_KEY;
+
+// get weather information
+export const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+    
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+};
+// get city information
+
+export const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+    
+    const response = await fetch(base + query);
+    const data = await response.json();
+    
+    return data[0];
+}
