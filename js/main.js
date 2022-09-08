4b15ea4719d99121e596a8eaa4e5dcaff7e049e7
Igor <w0rz@protonmail.com>
2022-09-08 20:44:41 -0300

    update day/night && icons

--- js/main.js
+++ js/main.js
@@ -3,12 +3,11 @@ import { getWeather, getCity } from './forecast';
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
-    // console.log(data);
-    // const cityDets = data.cityDets;
-    // const weather = data.weather;
-
+    console.log(data);
     // destructure properties
         const {cityDets, weather} = data
     // update details template
@@ -20,6 +19,19 @@ const updateUI = (data) => {
       <span>&deg;C</span>
     </div>
     `;
+
+    // update the night/day & icon images
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    let timeSrc = null;
+
+        if(weather.IsDayTime) {
+            timeSrc = 'img/day.svg';
+        } else {
+            timeSrc = 'img/night.svg';
+        }
+        time.setAttribute('src', timeSrc);
 
     // remove the d-none class if present
     if(card.classList.contains('d-none')) {
