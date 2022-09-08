7b527bf7b7e605fafa6fccd625c2d10cbce7f505
Igor <w0rz@protonmail.com>
2022-09-08 18:52:41 -0300

    details updated to dynamic && remove d-none class if present

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 import { getWeather, getCity } from './forecast';
 
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+    const cityDets = data.cityDets;
+    const weather = data.weather;
+
+    // update details template
+    details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+    `;
+
+    // remove the d-none class if present
+    if(card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+    }
+}
+
 const updateCity = async (city) => {
 
     const cityDets = await getCity(city);
@@ -19,6 +42,6 @@ cityForm.addEventListener('submit', e => {
 
     // update the ui with the new city
     updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
 } )
