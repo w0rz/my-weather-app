4a2123bc5871829321d8b2cf04c1d5d7c30b1414
Igor <w0rz@protonmail.com>
2022-09-08 19:02:35 -0300

    destructuring properties

--- js/main.js
+++ js/main.js
@@ -5,9 +5,12 @@ const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 
 const updateUI = (data) => {
-    const cityDets = data.cityDets;
-    const weather = data.weather;
+    // console.log(data);
+    // const cityDets = data.cityDets;
+    // const weather = data.weather;
 
+    // destructure properties
+        const {cityDets, weather} = data
     // update details template
     details.innerHTML = `
     <h5 class="my-3">${cityDets.EnglishName}</h5>
