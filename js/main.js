f03421796b592cdd76d2bba645891bd06f5cc5c4
Igor <w0rz@protonmail.com>
2022-09-08 20:52:17 -0300

    if statement updated to ternary operator

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
-    console.log(data);
     // destructure properties
         const {cityDets, weather} = data
     // update details template
@@ -24,15 +23,8 @@ const updateUI = (data) => {
     const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
     icon.setAttribute('src', iconSrc);
 
-    let timeSrc = null;
-
-        if(weather.IsDayTime) {
-            timeSrc = 'img/day.svg';
-        } else {
-            timeSrc = 'img/night.svg';
-        }
-        time.setAttribute('src', timeSrc);
-
+    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+    time.setAttribute('src', timeSrc);
     // remove the d-none class if present
     if(card.classList.contains('d-none')) {
     card.classList.remove('d-none');
